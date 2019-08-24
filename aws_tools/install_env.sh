21e8d4f6dc195d6a382ebd269bd0448302a14378
zhanjiefan <809428197@qq.com>
2019-08-24 16:56:30 +0800

    update env install

--- aws_tools/install_env.sh
+++ aws_tools/install_env.sh
@@ -3,10 +3,13 @@ curl -O https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
 bash Anaconda3-2018.12-Linux-x86_64.sh
 source ~/.bashrc
 conda install python=3.6
-conda install -c anaconda tensorflow-gpu 
-conda install pillow
-conda install -c menpo dlib
-conda install -c conda-forge opencv
+conda install -y -c anaconda tensorflow-gpu 
+conda install -y pillow
+# conda install -y -c menpo dlib
+# conda install -y -c conda-forge opencv
+pip install -U pip
+pip install -U setuptools
+pip install opencv-python cmake dlib face_recognition_models
 
 # install git
 sudo apt-get install git
